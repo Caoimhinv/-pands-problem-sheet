606ced047241f0134183d1f097680eb0d9696554
Caoimhinv <caoimhinvallely@gmail.com>
2021-02-09 15:25:19 +0000

    Week 02 Problem Task

--- bmi.py
+++ bmi.py
@@ -1,11 +1,15 @@
 # calculate BMI from inputted height and weight
 # Author: Caoimhin Vallely
 
-# ask user to enter their info
+# asks user to enter their info
 weight = float(input('Please enter your weight in kgs?'))
 height = float(input('Please enter your height in cms?'))
 
-# adapted formula (cms) for calculating BMI and rounded to 2 decimal places
-bmi = round(weight / (height ** 2) * 10000, 2)
+# converts height in cm to metres
+height_metres = height / 100
 
+# formula for calculating BMI and rounds to 2 decimal places
+bmi = round(weight / (height_metres ** 2), 2)
+
+# prints the result
 print('Your bmi is ' + str(bmi))
