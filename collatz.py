b0d78a0f324cf4547f9d955c6a5a40ee62a92ad7
Caoimhinv <caoimhinvallely@gmail.com>
2021-02-12 10:53:22 +0000

    Week 04 Problem Task

--- collatz.py
+++ collatz.py
@@ -3,21 +3,15 @@
 # by taking the current value and, if it is even,
 # divides it by two, but if it is odd, 
 # multiplies it by three and adds one.
-
 # The program ends if the current value is one.
 
 # Author: Caoimhin Vallely
 
-# creating a function (beyond me as of 11/2/21!!)
-def problemWeek4(number):
+number = int(input("Please enter a positive integer: ")) # asks user to enter number
+
+while (number != 1): # stops program when number hits 1
     if number % 2 == 0: # checking if number is even 
         number = number // 2 # dividing by 2
     else:
-        number = number * 3 + 1 # multiplying by 3 and adding 1
+        number = number * 3 + 1 # if not even, multiplying by 3 and adding 1
     print(number)
-    return(number)
-
-userNumber = int(input("Please enter a positive integer: ")) # asks user to enter number
-
-while (userNumber != 1): # stops programming when number hits 1
-    userNumber = problemWeek4(userNumber) # recalling function
