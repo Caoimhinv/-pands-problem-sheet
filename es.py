6a53c3078e68dfb8606e99b0510584f71edac3aa
Caoimhinv <caoimhinvallely@gmail.com>
2021-03-19 20:58:05 +0000

    update with error handling

--- es.py
+++ es.py
@@ -6,18 +6,27 @@
 
 # User types in programme name and file name in the command line
 # i.e. >>python es.py phadThai.txt<<
-# N.B. text file mneeds to be in the same folder as programme
+# N.B. text file needs to be in the same folder as program
 # I've included phadThai.txt here as an example text file
 
 # importing the sys module for accessing the text file
 import sys
 
 # accesses and allows the programme to 'read' the text file from the command line
-with open(sys.argv[1], 'rt') as textFile: # sys.argv[1] refers to 2 arguments in the command line,
-                                         # i.e. 1. programme (es.py) and 2. filename (e.g. phadThai.txt)
+try: # Error handling (exlpain more!)
+    with open(sys.argv[1], 'rt') as textFile: # sys.argv[1] refers to 2 arguments in the command line,
+                                         # i.e. 1. programme (es.py) and 2. filename (e.g. phadThai.txt) or file path
+                                         # (e.g /Users/caoimhinvallely/Desktop/GMIT Weekly Tasks/Pands-problem-sheet/phadThai.txt)
                                          # 'rt' lets us 'read' the file and specifices 'text' as opposed to binary
-    contents = textFile.read() # reads the content of the file
-    numberOfEs = contents.count("e") # uses 'count' method to count number of "e"s.
+        contents = textFile.read() # reads the content of the file and stores it in a variable 'contents'
+        numberOfEs = contents.count("e") # uses 'count' method to count number of "e"s.
                                     # I'm assuming that it's just lower case 'e' that is required
                                     # although including capital 'E' would be quite straightforward.
-    print ("There are {} 'e's in this file".format(numberOfEs)) # prints the result
+        print ("There are {} 'e's in this file".format(numberOfEs)) # prints the result
+        #print (sys.argv) # prints the arguments in the command line
+except IOError: # catches if incorrect file name or path entered
+    print("I can't seem to find your file? If it's in the same folder just enter <<filename.txt>>, if it's elsewhere enter the full path.")
+# except sys.argv < 2:
+#     print("hello")
+except IndexError: # catches if no file entered
+    print("Did you enter a file in the command line?")
